047e9b14efc19dfba86c7d6ebf749be8800b2b5a
Erdem Uslu <erdem@mobilex.com.tr>
2019-03-13 15:25:01 +0300

    add set&get method

--- src/index.js
+++ src/index.js
@@ -1,1 +1,44 @@
-console.log('test')
+// todo class
+class Todo {
+  constructor() {
+    // dom elements
+    this.form = document.querySelector('.todo-form');
+    this.input = document.getElementById('title');
+
+    // payload
+    this.todoArr = [];
+  }
+
+  get value() {
+    return this.input.value;
+  }
+
+  set list(title) {
+    if (title.length !== 0) {
+      return this.todoArr.push({ title })
+    }
+  }
+
+  saveItem() {
+    this.form.onsubmit = () => {
+      event.preventDefault();
+
+      this.list = this.value;
+
+      //
+      this.render();
+    }
+  }
+
+  render() {
+    console.log(this.todoArr);
+  }
+
+  init() {
+    // methods
+    this.saveItem();
+  }
+}
+
+// init todo class
+new Todo().init();
